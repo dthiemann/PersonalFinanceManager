9ac0b1a2548725ceaf7bb13b3c28745b2ed72f1e
Dylan Thiemann <dylanthiemann@gmail.com>
2015-06-29 20:45:23 -0600

    working on figuring stuff out

--- PersonalFinanceManager/Form1.Designer.cs
+++ PersonalFinanceManager/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PersonalFinanceManager {
+﻿using System.Drawing;
+
+namespace PersonalFinanceManager {
     partial class Form1 {
         /// <summary>
         /// Required designer variable.
@@ -23,12 +25,35 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 100);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This is a simple test label";
+            this.label1.Font = new Font(FontFamily.GenericSerif, 24); 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1864, 946);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
